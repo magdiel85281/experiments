9f0d4514ca0b67ed36bf927f5669aaa06f48b077
magdiel85281 <magdiel.delgadillo@gmail.com>
2019-12-11 22:10:48 -0700

    functionalized for testability

--- app/helloworld.py
+++ app/helloworld.py
@@ -1,7 +1,17 @@
 import os
 
 
-if __name__ == '__main__':
+def print_txt_file(message):
     with open('../data/outfile.txt', 'w') as f:
-        f.write('Hello World!')
+        f.write(message)
 
+
+def sysout_print(message):
+    print(message)
+
+
+if __name__ == '__main__':
+    text_out = 'Hello World!'
+
+    print_txt_file(text_out)
+    sysout_print(text_out)
